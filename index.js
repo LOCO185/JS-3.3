6295b459fc9a56e4def58f5c0acec77a9136fdc0
LOCO185 <jloco151@gmail.com>
2021-11-08 21:56:22 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+function percentageOfWorld1(population) {
+    return (population / 7900) * 100;
+}
+console.log(percentageOfWorld1 (1441));//china
+
+console.log(percentageOfWorld1 (8));//israel
+
+console.log(percentageOfWorld1 (60));//germany
+
+const percentageOfWorld2 = (population) => (population / 7900 )*100 ;
+
+console.log(percentageOfWorld2 (140));//brazil
+
+console.log(percentageOfWorld2 (40));//italy
+
+console.log(percentageOfWorld2 (87));//spain
+
